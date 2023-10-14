e8888b38cb0ee230e1d00f1e9a9b91f837a2641f
Sergii Tatianchenko <taurus090@gmail.com>
2023-10-14 19:23:11 +0300

    add form

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,25 +1,45 @@
 import { Link } from 'react-router-dom';
 import s from './navigation.module.scss';
+import { useSelector } from 'react-redux';
 
 const Navigation = () => {
+  const token = useSelector(state => state.profile.token);
+  let navData = [
+    {
+      text: 'Contacts',
+      link: '/contacts',
+      mustToken: true,
+    },
+    {
+      text: 'Login',
+      link: '/login',
+      mustToken: false,
+    },
+    {
+      text: 'Register',
+      link: '/register',
+      mustToken: false,
+    },
+  ];
+  if (token !== null) {
+    navData = navData.filter(el => el.mustToken);
+  } else {
+    navData = navData.filter(el => !el.mustToken);
+  }
+  console.log(navData);
+  const LinkCollection = () => {
+    return navData.map(el => (
+      <li key={el.text} className={s.nav_list_item}>
+        <Link className={s.nav_list_item_link} to={el.link}>
+          {el.text}
+        </Link>
+      </li>
+    ));
+  };
   return (
     <nav className={s.nav}>
       <ul className={s.nav_list}>
-        <li className={s.nav_list_item}>
-          <Link className={s.nav_list_item_link} to="/contacts">
-            Contacts
-          </Link>
-        </li>
-        <li className={s.nav_list_item}>
-          <Link className={s.nav_list_item_link} to="/login">
-            Login
-          </Link>
-        </li>
-        <li className={s.nav_list_item}>
-          <Link className={s.nav_list_item_link} to="/register">
-            Register
-          </Link>
-        </li>
+        <LinkCollection />
       </ul>
     </nav>
   );
--- src/components/userMenu/UserMenu.jsx
+++ src/components/userMenu/UserMenu.jsx
@@ -1,13 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux';
 import s from './userMenu.module.scss';
 import { logoutData } from 'redux/slices/profile';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 const UserMenu = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
   const token = useSelector(state => state.profile.token);
   const email = useSelector(state => state.profile.user.email);
   const handleLogOut = () => {
+    localStorage.removeItem('token');
     dispatch(logoutData(token));
   };
+  useEffect(() => {
+    if (token === null) {
+      if (location.pathname === '/contacts') {
+        navigate('/login');
+      }
+    }
+  }, [token, navigate, location.pathname]);
+
   if (token === null) {
     return null;
   }
