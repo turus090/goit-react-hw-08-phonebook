19013ea42d371e60d0d15ca16e14923660dc26b0
Sergii Tatianchenko <taurus090@gmail.com>
2023-10-22 13:52:41 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Contacts from 'page/contacts/Contacts';
-import Login from 'page/login/Login ';
-import Register from 'page/register/Register';
+import ContactsPage from 'page/contacts/Contacts';
+import LoginPage from 'page/login/Login ';
+import RegisterPage from 'page/register/Register';
 import RestrictedRoute from './RestrictedRoute';
 import Layaout from './layaout';
+import HomePage from 'page/home/HomePage';
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layaout />}>
+          <Route index element={<HomePage />} />
           <Route
-            index
             path="/contacts"
             element={
-              <RestrictedRoute redirectTo="/login" component={<Contacts />} />
+              <RestrictedRoute
+                redirectTo="/login"
+                component={<ContactsPage />}
+              />
             }
           />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" index element={<Register />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Route>
       </Routes>
     </>
---
+++ src/page/home/HomePage.jsx
@@ -0,0 +1,6 @@
+import s from './home.module.scss';
+const HomePage = () => {
+  return <h1 className={s.title}>HomePage</h1>;
+};
+
+export default HomePage;
