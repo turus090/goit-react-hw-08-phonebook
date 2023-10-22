50ffa7e83753c66df4e6d39ae5c14241ea8aa9f4
Sergii Tatianchenko <taurus090@gmail.com>
2023-10-22 22:41:43 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,28 +4,25 @@ import ContactsPage from 'page/contacts/Contacts';
 import LoginPage from 'page/login/Login ';
 import RegisterPage from 'page/register/Register';
 import RestrictedRoute from './RestrictedRoute';
-import Layaout from './layaout';
 import HomePage from 'page/home/HomePage';
+import Layout from './Layout';
+
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layaout />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/contacts"
-            element={
-              <RestrictedRoute
-                redirectTo="/login"
-                component={<ContactsPage />}
-              />
-            }
-          />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="contacts"
+          element={
+            <RestrictedRoute redirectTo="/login" element={<ContactsPage />} />
+          }
+        />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 };
+
 export default App;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
+import React from 'react';
 import Navigation from './navigation/Navigation';
 import UserMenu from './userMenu/UserMenu';
+import { Outlet } from 'react-router-dom';
 
-const Layaout = () => {
+const Layout = () => {
   return (
     <>
       <Navigation />
       <UserMenu />
+      <Outlet />
     </>
   );
 };
 
-export default Layaout;
+export default Layout;
